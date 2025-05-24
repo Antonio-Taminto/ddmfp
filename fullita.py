0a7077f7b7aeca17f25bcbce86595fb133b41d85
Antonio-Taminto <63263822+Antonio-Taminto@users.noreply.github.com>
2025-05-24 10:53:18 +0200

    Update fullita.py

--- fullita.py
+++ fullita.py
@@ -11,8 +11,8 @@ from bs4 import BeautifulSoup
 import time
 
 
-MFPLINK = ""     # non mettere lo / finale al link
-MFPPSW = ""
+MFPLINK = "https://antonio1028-nomefantasioso.hf.space"     # non mettere lo / finale al link
+MFPPSW = "stremio"
 
 # Constants
 REFERER = "forcedtoplay.xyz"
