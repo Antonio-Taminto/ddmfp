5aa28ef7b27a16b9849d53692519e17add19439a
Antonio-Taminto <63263822+Antonio-Taminto@users.noreply.github.com>
2025-05-24 10:53:57 +0200

    Update onlyevents.py

--- onlyevents.py
+++ onlyevents.py
@@ -9,8 +9,8 @@ import requests
 from bs4 import BeautifulSoup
 import time
 
-MFPLINK = ""     # non mettere lo / finale al link
-MFPPSW = ""
+MFPLINK = "https://antonio1028-nomefantasioso.hf.space"     # non mettere lo / finale al link
+MFPPSW = "stremio"
 
 # Costanti
 NUM_CHANNELS = 10000
